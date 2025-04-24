912cd063aaa558e7d805712d94e8ca1ebfe3ce38
Yochem van Rosmalen <git@yochem.nl>
2025-04-24 16:04:21 +0200

    Cheeky LP logo spin on menu popup

--- static/js/hamburger.js
+++ static/js/hamburger.js
@@ -1,4 +1,6 @@
-document.querySelector('.menu').addEventListener('click', function() {
+document.querySelector('.hamburger').addEventListener('click', function() {
 	this.classList.toggle('cross')
-	document.querySelector('nav').classList.toggle('active')
+	document.querySelector('nav.menu').classList.toggle('active')
+	const img = document.querySelector('img');
+	img.style.transform = this.classList.contains('cross') ? 'rotate(720deg)' : '';
 })
