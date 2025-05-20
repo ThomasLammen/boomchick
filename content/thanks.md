9724b5ca9f603cf29924be67f37e1f4bc375aca3
Yochem van Rosmalen <git@yochem.nl>
2025-05-20 15:25:53 +0200

    Disable next button

--- content/thanks.md
+++ content/thanks.md
@@ -1,4 +1,12 @@
 ---
-title: Thanks
+title: Bedankt!!
 layout: thanks
+params:
+  themes:
+    - title: latin-vibes
+      spotify: |
+        <iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/1tJmVBavhzhCvTAYHKZ6Rn?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+    - title: lorem-ipsum
+      spotify: |
+        <iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/1tJmVBavhzhCvTAYHKZ6Rn?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
 ---
--- static/js/form.js
+++ static/js/form.js
@@ -4,12 +4,25 @@ function handleGoto(value) {
   document.querySelectorAll('[id]').forEach(el => {
     if (regex.test(el.id)) {
       if (el.id === value) {
-        el.classList.remove('hidden');
+        // el.classList.remove('hidden');
+        el.disabled = false;
       } else {
-        el.classList.add('hidden');
+        // el.classList.add('hidden');
+        el.disabled = true;
       }
     }
   });
+}
+
+function enableNextButton(input) {
+  const fieldset = input.closest('fieldset')
+  const button = fieldset.querySelector('.next')
+  if (button) {
+    button.disabled = input.value.trim() === ''
+  }
+  window.onbeforeunload = function() {
+      return true;
+  };
 }
 
 function scrollQuestionPage(current, direction) {
@@ -34,4 +47,19 @@ function nextPage(current) {
 
 function prevPage(current) {
   scrollQuestionPage(current, -1);
+}
+
+function thanksPage(name, change_func) {
+  const el = document.querySelector('input[name="_next"]')
+  const url = new URL(el.value);
+  url.searchParams.set(name, change_func(url.searchParams.get(name)));
+  el.value = url.toString();
+}
+
+function addCost(cost) {
+  thanksPage('cost', (x) => Number(x) + cost);
+}
+
+function setTheme(theme) {
+  thanksPage('theme', (_) => theme);
 }
