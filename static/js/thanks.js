7f2fd3ff932bd01524800e9212ccafa0d1f416fa
Yochem van Rosmalen <git@yochem.nl>
2025-09-26 14:12:18 +0200

    More fixes

--- static/js/thanks.js
+++ static/js/thanks.js
@@ -1,10 +1,19 @@
-const params = new URLSearchParams(window.location.search)
+const params = new URLSearchParams(window.location.search);
 
 if (params.get('theme')) {
-	const h1 = document.createElement('h1');
-	h1.classList.add('theme');
-	h1.innerText = params.get('theme');
-	document.querySelector('main section ul').insertAdjacentElement('beforebegin', h1);
+	const input_theme = params.get('theme');
+	for (var theme of window.themes) {
+		if (theme.name == input_theme) {
+			const c = document.createElement('iframe');
+			c.setAttribute('src', `https://open.spotify.com/embed/album/${theme.spotify}`);
+			c.setAttribute('width', '70%');
+			c.setAttribute('height', '200');
+			c.setAttribute('frameborder', '0');
+			c.setAttribute('allowtransparency', 'true');
+			c.setAttribute('allow', 'encrypted-media');
+			document.querySelector('main section ul').insertAdjacentElement('beforebegin', c);
+		}
+	}
 }
 
 if (params.get('cost')) {
@@ -13,11 +22,3 @@ if (params.get('cost')) {
 	c.innerHTML = `Perfecte live muziek voor <em>€${Math.floor(cost)},-</em>`
 	document.querySelector('main section ul').insertAdjacentElement('afterend', c);
 }
-
-// const list = document.querySelector('main section ul');
-// for (const [key, value] of params.entries()) {
-// 	console.log({key: value});
-// 	const item = document.createElement('li')
-// 	item.textContent = `${key}: ${value}`
-// 	list.appendChild(item)
-// }
